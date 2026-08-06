fbd606954bc5b5a3822e3bfb4358355699948d82
JanisPeretsons83 <144663195+JanisPeretsons83@users.noreply.github.com>
2026-08-06 22:39:54 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = "inventory-app-v1-72"; // 🔥 MAINI ŠO katru update!
+const CACHE_NAME = "inventory-app-v10-1"; // 🔥 MAINI ŠO katru update!
 
 const BASE = "/Inventory-app";
 const urlsToCache = [
