9ce2173311c4397cf3ab5c5e758f26b175591168
JanisPeretsons83 <144663195+JanisPeretsons83@users.noreply.github.com>
2026-08-08 20:32:30 +0300

    Update cache version and maintain service worker logic

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = "inventory-app-TestMod-v1-10"; // 🔥 MAINI ŠO katru update!
+const CACHE_NAME = "inventory-app-TestMod-v1-11"; // 🔥 MAINI ŠO katru update!
 
 const BASE = "/Inventory-app-TestMod";
 const urlsToCache = [
